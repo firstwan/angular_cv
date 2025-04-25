efce82db6c5e2acd672baa0f6afa85c06d210cee
First Wan <siewyik85@hotmail.com>
2025-04-25 21:07:32 +0800

    Add git repo redirection

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
+import { simpleGithub } from '@ng-icons/simple-icons'
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NarbarComponent {
+  githubUrl = "https://github.com/firstwan/firstwan.github.io";
+  githubIcon = simpleGithub;
 }
